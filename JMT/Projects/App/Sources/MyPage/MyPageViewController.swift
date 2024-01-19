b6ad97e7f0f8e74b9bb1aa4853a5cfd12a8cdb7d
hooni <jhlee909090@gmail.com>
2024-01-19 19:11:00 +0900

    style: Segment Custom 성공

--- JMT/Projects/App/Sources/MyPage/MyPageViewController.swift
+++ JMT/Projects/App/Sources/MyPage/MyPageViewController.swift
@@ -16,33 +16,59 @@ class MyPageViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var dataSource: [DummyDataType] = []
 
     
+    @IBOutlet weak var HeaderView: UIView!
+    
     @IBOutlet weak var MyPageSegment: UISegmentedControl!
     @IBOutlet weak var MypageMainTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         MypageMainTableView.delegate = self
         MypageMainTableView.dataSource = self
 
         updateDataSource(segmentIndex: MyPageSegment.selectedSegmentIndex)
+        
+        
+        self.MyPageSegment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.gray], for: .normal)
+            self.MyPageSegment.setTitleTextAttributes(
+              [
+                NSAttributedString.Key.foregroundColor: UIColor.green,
+                .font: UIFont.systemFont(ofSize: 13, weight: .semibold)
+              ],
+              for: .selected
+            )
+            self.MyPageSegment.selectedSegmentIndex = 0
     }
+    
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
             updateDataSource(segmentIndex: sender.selectedSegmentIndex)
         MypageMainTableView.reloadData()
         
         }
     
+    
+
+    
     func updateDataSource(segmentIndex: Int) {
            if segmentIndex == 0 {
                dataSource = [
-                   DummyDataType(image: "image1", labelText: "레스토랑 1"),
-                   DummyDataType(image: "image1", labelText: "레스토랑 2"),
-                   DummyDataType(image: "image1", labelText: "레스토랑 3"),
+                DummyDataType(image: "image1", labelText: "레스토랑 1"),
+                DummyDataType(image: "image1", labelText: "레스토랑 2"),
+                DummyDataType(image: "image1", labelText: "레스토랑 3"),
+                DummyDataType(image: "image1", labelText: "레스토랑 3"),
                ]
-           } else {
+           } else if segmentIndex == 1 {
                dataSource = [
-                DummyDataType(image: "image1", labelText: "레스토랑 4"),
+                DummyDataType(image: "image1", labelText: " 4"),
+                DummyDataType(image: "image1", labelText: "레스토랑 5"),
+                DummyDataType(image: "image1", labelText: "레스토랑 6"),
+               ]
+           } else if segmentIndex == 2 {
+               dataSource = [
+                DummyDataType(image: "image1", labelText: " 5"),
                 DummyDataType(image: "image1", labelText: "레스토랑 5"),
                 DummyDataType(image: "image1", labelText: "레스토랑 6"),
                ]
