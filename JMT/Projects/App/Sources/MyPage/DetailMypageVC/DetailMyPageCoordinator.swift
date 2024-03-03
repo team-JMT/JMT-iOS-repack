5352d2442b2430ee9869cbb9912043854a08e83b
hooni <jhlee909090@gmail.com>
2024-03-03 16:28:09 +0900

    feat: image Upload api

--- JMT/Projects/App/Sources/MyPage/DetailMypageVC/DetailMyPageCoordinator.swift
+++ JMT/Projects/App/Sources/MyPage/DetailMypageVC/DetailMyPageCoordinator.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 protocol DetailMyPageCoordinator: Coordinator {
     //
@@ -111,12 +112,6 @@ class DefaultDetailMyPageCoordinator: DetailMyPageCoordinator {
         coordinator.start()
     }
     
-    //이미지 등록
-    func setProfileImagePopupCoordinator() {
-        let coordinator = DefaultProfileImagePopupCoordinator(navigationController: navigationController, parentCoordinator: self, finishDelegate: self)
-        childCoordinators.append(coordinator)
-    }
-    
     //닉네임 수정
     func setMyPageChangeNicknameCoordinator() {
         let coordinator = DefaultMyPageChangeNicknaemCoordinator(navigationController: navigationController)
@@ -132,6 +127,12 @@ class DefaultDetailMyPageCoordinator: DetailMyPageCoordinator {
         coordinator.start()
     }
     
+    
+    //이미지 등록
+    func setProfileImagePopupCoordinator() {
+        let coordinator = DefaultProfileImagePopupCoordinator(navigationController: navigationController, parentCoordinator: self, finishDelegate: self)
+        childCoordinators.append(coordinator)
+    }
     
     func showProfileImagePopupViewController() {
         
@@ -163,11 +164,42 @@ class DefaultDetailMyPageCoordinator: DetailMyPageCoordinator {
         if let detailMyPageViewController = self.navigationController?.topViewController as? DetailMyPageVC {
             detailMyPageViewController.profileImage.image = image
             detailMyPageViewController.viewModel?.isDefaultProfileImage = isDefault
+
+            uploadProfileImage(image: (image ?? UIImage(named: "DefaultImage"))!)
+            NotificationCenter.default.post(name: NSNotification.Name("ProfileImageUpdated"), object: nil)
+
         }
     }
     
-    
+    func uploadProfileImage(image: UIImage) {
+        
+        guard let accessToken = DefaultKeychainService.shared.accessToken,
+              let imageData = image.jpegData(compressionQuality: 0.5) else {
+            print("Access Token or Image Data is not available")
+            return
+        }
 
+
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(accessToken)",
+            "Accept": "*/*"
+        ]
+
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imageData, withName: "profileImg", fileName: "profile.jpg", mimeType: "image/jpeg")
+        }, to: "https://api.jmt-matzip.dev/api/v1/user/profileImg", method: .post, headers: headers)
+        .responseDecodable(of: ImageResponse.self) { response in
+            switch response.result {
+            case .success(let responseData):
+                print("Image uploaded successfully: \(responseData.message)")
+            case .failure(let error):
+                print("Failed to upload image: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    
+    
 //배열내에 있는 코디네이터에 enum으로 선언한 애가 있는지
     func getChildCoordinator(_ type: CoordinatorType) -> Coordinator? {
         var childCoordinator: Coordinator? = nil
