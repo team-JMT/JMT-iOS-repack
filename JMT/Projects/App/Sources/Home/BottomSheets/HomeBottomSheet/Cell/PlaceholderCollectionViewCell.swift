d6603281866dea207b89d67d2b241f5efdb8f292
hooni <jhlee909090@gmail.com>
2024-03-05 17:27:51 +0900

    폴더 경로문제 해결

---
+++ JMT/Projects/App/Sources/Home/BottomSheets/HomeBottomSheet/Cell/PlaceholderCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderCollectionViewCell.swift
+//  JMTeng
+//
+//  Created by PKW on 2024/02/28.
+//
+
+import UIKit
+
+class PlaceholderCollectionViewCell: UICollectionViewCell {
+    
+   
+   
+    override func awakeFromNib() {
+       
+    }
+}
---
+++ JMT/Projects/App/Sources/MyPage/ServiceUserPrivacy/ServiceUser/ServiceUseViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  321312.swift
+//  JMTeng
+//
+//  Created by 이지훈 on 2/22/24.
+//
+
+import Foundation
+
+class ServiceUseViewModel {
+    
+    weak var coordinator: ServiceUseCoordinator?
+    
+    
+    let test = "홈"
+}
