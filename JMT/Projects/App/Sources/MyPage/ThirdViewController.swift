0c8e68ba7291cd01ba9b16bdb9f695d67617cba7
hooni <jhlee909090@gmail.com>
2024-03-05 19:07:33 +0900

    오류 1

--- JMT/Projects/App/Sources/MyPage/ThirdViewController.swift
+++ JMT/Projects/App/Sources/MyPage/ThirdViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondSegmentViewController.swift
+//  ThirdViewController.swift
 //  JMTeng
 //
 //  Created by 이지훈 on 2/16/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondSegmentViewController: UIViewController {
+class ThirdSegmentViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
