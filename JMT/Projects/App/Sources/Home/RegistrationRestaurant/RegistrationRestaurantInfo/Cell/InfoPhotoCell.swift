9573bd3add055aa35824c0aae455e858f15da9da
hooni <jhlee909090@gmail.com>
2024-03-05 17:27:51 +0900

    폴더 경로문제 해결

---
+++ JMT/Projects/App/Sources/Home/RegistrationRestaurant/RegistrationRestaurantInfo/Cell/InfoPhotoCell.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoCell.swift
+//  JMTeng
+//
+//  Created by PKW on 2024/02/05.
+//
+
+import UIKit
+
+protocol InfoPhotoCellDelegate: AnyObject {
+    func didTabDeleteButton(in cell: UICollectionViewCell)
+}
+
+class InfoPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    weak var delegate: InfoPhotoCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        menuImageView.layer.cornerRadius = 8
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+    }
+    
+    @IBAction func didTabDeleteButton(_ sender: UIButton) {
+        delegate?.didTabDeleteButton(in: self)
+    }
+}
