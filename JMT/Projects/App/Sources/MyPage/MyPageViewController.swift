06994156f1c9894a7138b368bd82ee150284f66d
hooni <jhlee909090@gmail.com>
2024-03-05 14:41:31 +0900

    fix: gotoDetail 액션 수정

--- JMT/Projects/App/Sources/MyPage/MyPageViewController.swift
+++ JMT/Projects/App/Sources/MyPage/MyPageViewController.swift
@@ -48,9 +48,9 @@ class MyPageViewController: UIViewController, UIScrollViewDelegate
     }
     
     
-    let fixedHeaderViewHeight: CGFloat = 150 // 고정될 뷰의 높이
+    let fixedHeaderViewHeight: CGFloat = 80 // 고정될 뷰의 높이
     let triggerOffset: CGFloat = 150 // 고정 뷰가 나타나기 시작할 스크롤 위치
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,25 +58,28 @@ class MyPageViewController: UIViewController, UIScrollViewDelegate
         setupSegmentedControl()
         setupFixedHeaderView()
         switchToViewController(at: MyPageSegment.selectedSegmentIndex)
-
         
     }
     
     
     func setupFixedHeaderView() {
-           // 고정된 헤더 뷰 설정
-           fixedHeaderView.frame = CGRect(x: 0, y: -fixedHeaderViewHeight, width: view.frame.width, height: fixedHeaderViewHeight)
-           fixedHeaderView.isHidden = true // 초기 상태에서는 숨김 처리
-           
-           // 고정된 헤더 뷰 내에 UnderlineSegmentedControl 추가
-           fixedSegmentedControl = UnderlineSegmentedControl(items: ["First", "Second", "Third"])
-           fixedSegmentedControl.frame = CGRect(x: 10, y: 5, width: fixedHeaderView.frame.width - 20, height: fixedHeaderView.frame.height - 10)
-           fixedSegmentedControl.selectedSegmentIndex = 0
-           fixedSegmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
-           fixedHeaderView.addSubview(fixedSegmentedControl)
-           
-           view.addSubview(fixedHeaderView)
-       }
+        fixedHeaderView.isHidden = true
+        fixedHeaderView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: fixedHeaderViewHeight)
+        view.addSubview(fixedHeaderView)
+        
+        // Calculate the y position so the segmented control is at the bottom of the fixedHeaderView
+        let segmentedControlHeight: CGFloat = 31 // Fixed height of the segmented control
+        let yPosition = fixedHeaderViewHeight - segmentedControlHeight // Position at the bottom
+        
+        // Initialize the fixedSegmentedControl with the calculated y position
+        fixedSegmentedControl = UnderlineSegmentedControl(items: ["등록한 맛집", "좋아한 맛집", "나의 후기"])
+        fixedSegmentedControl.frame = CGRect(x: 10, y: yPosition, width: fixedHeaderView.frame.width - 20, height: segmentedControlHeight)
+        fixedSegmentedControl.selectedSegmentIndex = MyPageSegment.selectedSegmentIndex
+        fixedSegmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        fixedHeaderView.addSubview(fixedSegmentedControl)
+    }
+
+
     
     // 세그먼트 컨트롤 설정 및 액션 추가
     private func setupSegmentedControl() {
@@ -85,13 +88,13 @@ class MyPageViewController: UIViewController, UIScrollViewDelegate
     
     // 세그먼트 컨트롤 값 변경 시 호출될 메서드
     @objc private func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-        switchToViewController(at: sender.selectedSegmentIndex)
-        
-        
-        // 동기화: 메인 세그먼트 컨트롤과 고정 세그먼트 컨트롤의 선택을 일치시킴
-                MyPageSegment.selectedSegmentIndex = sender.selectedSegmentIndex
-                fixedSegmentedControl.selectedSegmentIndex = sender.selectedSegmentIndex
+        let selectedIndex = sender.selectedSegmentIndex
+        MyPageSegment.selectedSegmentIndex = selectedIndex
+        fixedSegmentedControl.selectedSegmentIndex = selectedIndex
+        switchToViewController(at: selectedIndex)
     }
+
+
     
     // 주어진 인덱스에 해당하는 뷰 컨트롤러로 전환
     private func switchToViewController(at index: Int) {
@@ -143,15 +146,20 @@ class MyPageViewController: UIViewController, UIScrollViewDelegate
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let yOffset = scrollView.contentOffset.y
         if yOffset >= triggerOffset {
-            // 스크롤이 triggerOffset 이상일 때
+            // 스크롤이 triggerOffset 이상일 때 고정 헤더 뷰를 표시하고 상단에 고정
             fixedHeaderView.isHidden = false
             fixedHeaderView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: fixedHeaderViewHeight)
-            scrollView.contentInset.top = fixedHeaderViewHeight // 상단 고정 뷰의 높이만큼 스크롤 뷰의 상단 인셋 조정
+            view.bringSubviewToFront(fixedHeaderView) // 뷰 계층에서 가장 앞으로 가져옴
+            scrollView.contentInset.top = fixedHeaderViewHeight // 고정 헤더 뷰 높이만큼 상단 인셋 조정
         } else {
-            // 스크롤이 triggerOffset 미만일 때
+            // 스크롤이 triggerOffset 미만일 때 고정 헤더 뷰 숨김
             fixedHeaderView.isHidden = true
-            scrollView.contentInset.top = 0
+            scrollView.contentInset.top = 0 // 상단 인셋 초기화
         }
+    }
+    
+    @IBAction func goToDetail(_ sender: Any) {
+        viewModel?.coordinator?.goToDetailMyPageView()
     }
     
     
