bcd3c195b929f5b3578d7337e84447e73fc481f2
KWiOS0101 <72461214+KiWooPark@users.noreply.github.com>
2024-03-08 20:13:13 +0900

    Revert "fix: 바텀시트 데이터 로딩 딜레이 삭제"

--- JMT/Projects/App/Sources/Home/BottomSheets/HomeBottomSheet/HomeBottomSheetViewController.swift
+++ JMT/Projects/App/Sources/Home/BottomSheets/HomeBottomSheet/HomeBottomSheetViewController.swift
@@ -47,32 +47,17 @@ class HomeBottomSheetViewController: UIViewController {
         
         viewModel?.didUpdateBottomSheetTableView = {
             
-            self.viewModel?.isLodingData = false
-            
             DispatchQueue.main.async {
+
                 self.hiddenBottomSheetButton()
                 self.bottomSheetCollectionView.reloadData()
                 self.bottomSheetCollectionView.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(1))
             }
         }
         
-        viewModel?.didUpdateFilterRestaurants = {
         
-            self.viewModel?.isLodingData = true
-            self.bottomSheetCollectionView.showAnimatedGradientSkeleton()
-            
-            Task {
-                do {
-                    try await self.viewModel?.fetchGroupRestaurantsAsync()
-                    self.viewModel?.isLodingData = false
-                    
-                    self.bottomSheetCollectionView.reloadData()
-                    self.bottomSheetCollectionView.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(1))
-                    
-                } catch {
-                    print(error)
-                }
-            }
+        viewModel?.didUpdateIndex = { index in
+            self.bottomSheetCollectionView.scrollToItem(at: IndexPath(row: index, section: 1), at: .top, animated: true)
         }
     }
     
